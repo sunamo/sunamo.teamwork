2e7fc339f2aca5a678cf7dbeac0c6cf2e7a6d0db
Radek Jančík <sunamocz@outlook.com>
2019-12-19 18:56:54 +0100

    Mass commit 19-12-2019

---
+++ SunamoSlnGen/SunamoSlnGenHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Construction;
+using SlnGen.Common;
+
+public class SunamoSlnGenHelper
+    {
+    /// <summary>
+    /// Working wit Microsoft.Build.Construction.SolutionFile which cant add new files
+    /// </summary>
+    /// <param name="solutionFilePath"></param>
+    /// <returns></returns>
+    public static List<string> GetProjects(string solutionFilePath)
+    {
+        SolutionFile solutionFile = SolutionFile.Parse(solutionFilePath);
+
+
+        var p = solutionFile.ProjectsByGuid;
+        return p.Select(d => d.Value.AbsolutePath).ToList();   
+    }
+
+    public static List<string> s()
+    {
+        SlnFile sln = new SlnFile();
+        sln.Lo
+    }
+}
+
---
+++ SunamoSlnGen/readme.txt
@@ -0,0 +1,2 @@
+﻿Must use my project, not nuget package because some class (like SlnProject) is not available with nuget joined
+SlnGen can't load and write files at one. Microsoft.Build.Construction.SolutionFile allow only load and SlnGen.Common.SlnFile only save
